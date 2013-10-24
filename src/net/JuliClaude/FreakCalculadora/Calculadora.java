6c242ea8eee6b2bb79b84a7bbcc91427ac96c198
Juli <juli.fer14@gmail.com>
2013-10-24 16:25:36 +0200

    Modifico main per a cridar operacions

--- src/net/JuliClaude/FreakCalculadora/Calculadora.java
+++ src/net/JuliClaude/FreakCalculadora/Calculadora.java
@@ -6,18 +6,45 @@ public class Calculadora {
 	/*
 	 * Calculadora xaxi guai que no serveix per a res. Plena de bugs!
 	 */
+	Scanner lector = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+		Operacions operar = new Operacions();
 		Calculadora c = new Calculadora();
-		float numero1 = c.demanarNumero();
-		float numero2 = c.demanarNumero();
-		System.out.println(numero1 + "" + numero2);
+		float num1 = c.demanarNumero();
+		float num2 = c.demanarNumero();
+		int opcio = 0;
+		System.out.println("Indica quina operació vols realitzar");
+		System.out.println("1- Sumar");
+		System.out.println("2-Restar");
+		System.out.println("3-Dividir");
+		System.out.println("4-Multiplicar");
+		System.out.println("5-Convertir a binari");
+		opcio = lector.nextInt();
+		switch (opcio) {
+		case 1:
+			operar.suma(num1, num2);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	public float demanarNumero() {
-		Scanner lector = new Scanner(System.in);
+		// Scanner lector = new Scanner(System.in);
 		System.out.println("Escriu un número");
 		float numero = lector.nextFloat();
 		return numero;
+	}
+
+	public void esborrarNumeros(float n1, float n2) {
+		// Scanner lector = new Scanner(System.in);
+		Esborrar sup = new Esborrar(n1, n2);
+		System.out.println("Indica quin número vols esborrar");
+		sup.mostrarNumeros();
+		float num = lector.nextFloat();
+		sup.esborrarNumero(num);
 	}
 }
--- src/net/JuliClaude/FreakCalculadora/Esborrar.java
+++ src/net/JuliClaude/FreakCalculadora/Esborrar.java
@@ -3,9 +3,28 @@ package net.JuliClaude.FreakCalculadora;
 public class Esborrar {
 	float num1;
 	float num2;
+	
 	public Esborrar(float n1, float n2){
 		num1 = n1;
 		num2 = n2;
+	}
+	
+	public void mostrarNumeros(){
+		System.out.println("Els números són: ");
+		System.out.println("Número 1: " + num1);
+		System.out.println("Número 2: " + num2);
+	}
+	
+	public void esborrarNumero(float num){
+		if(num1 == num){
+			num1 = 0;
+			System.out.println("Número esborrat correctament");
+		}else if(num2 == num){
+			num2 = 0;
+			System.out.println("Número esborrat correctament");
+		}else{
+			System.out.println("El número entrat no és correcte");
+		}
 	}
 	
 }
