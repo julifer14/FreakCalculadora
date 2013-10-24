86f001228d0bd4d5bf019a3d1776a9ee52a0558c
Juli <juli.fer14@gmail.com>
2013-10-24 15:40:39 +0200

    Activitat 2

--- src/net/JuliClaude/FreakCalculadora/Calculadora.java
+++ src/net/JuliClaude/FreakCalculadora/Calculadora.java
@@ -1,7 +1,23 @@
 package net.JuliClaude.FreakCalculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 /*
- * Comentari :D
+ * Calculadora xaxi guai que no serveix per a res. Plena de bugs!
  */
+	
+	public static void main(String[]args){
+		Calculadora c  =  new Calculadora();
+		int numero1 = c.demanarNumero();
+		int numero2 = c.demanarNumero();
+		System.out.println(numero1 +""+numero2);
+	}
+	
+	public int demanarNumero(){
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Escriu un número");
+		int numero = lector.nextInt();
+		return numero;
+	}
 }
