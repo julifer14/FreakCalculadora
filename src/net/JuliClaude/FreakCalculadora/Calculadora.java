c2d0d9a0567cc658a3bcb3d207c03cd12b4a360b
Juli <juli.fer14@gmail.com>
2013-10-24 15:49:07 +0200

    Afegeixo el fitxer esborrar. S'encarregarà d'esborrar els números que indiqui l'usuari

--- src/net/JuliClaude/FreakCalculadora/Calculadora.java
+++ src/net/JuliClaude/FreakCalculadora/Calculadora.java
@@ -3,18 +3,18 @@ package net.JuliClaude.FreakCalculadora;
 import java.util.Scanner;
 
 public class Calculadora {
-/*
- * Calculadora xaxi guai que no serveix per a res. Plena de bugs!
- */
-	
-	public static void main(String[]args){
-		Calculadora c  =  new Calculadora();
+	/*
+	 * Calculadora xaxi guai que no serveix per a res. Plena de bugs!
+	 */
+
+	public static void main(String[] args) {
+		Calculadora c = new Calculadora();
 		int numero1 = c.demanarNumero();
 		int numero2 = c.demanarNumero();
-		System.out.println(numero1 +""+numero2);
+		System.out.println(numero1 + "" + numero2);
 	}
-	
-	public int demanarNumero(){
+
+	public int demanarNumero() {
 		Scanner lector = new Scanner(System.in);
 		System.out.println("Escriu un número");
 		int numero = lector.nextInt();
